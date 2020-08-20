f9bfcf4dfbbc6e8d97e0890ae4a12c4a16f2ef07
mausfer <mauriciosfernandes@gmail.com>
2020-08-20 17:18:04 -0300

    First Model

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,20 +7,21 @@ const helmet = require('helmet'); // Auxilia nas máscaras dos headers
 const routes = require('./routes'); // Importa o módulo de rotas
 const logger = require('./helper/logger'); // Formata s logs de forma mais profissional
 
-const app = express(); //Instancia a função express que permite a utilização mais fácil das funções do express.
+const app = express(); // Instancia a função express que permite a utilização mais fácil das funções do express.
 
 app.use(helmet()); // Define as máscaras das requisições
 app.use(cors()); // Instancia origem de requisições além das API fakes
 app.use(express.json()); // Define req e res em formato de json
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan('dev'));
 
-app.use((error, req, res, next) =>{
-    logger.error(error);
-    return res.status(500).json({ erro: "Houve um erro na API"})
+app.use((error, req, res, next) => {
+  logger.error(error);
+  return res.status(500).json({ erro: 'Houve um erro na API' });
 });
 
-app.listen(process.env.PORT || 3001), () =>{
-    logger.infor(`API RODANDO NA PORTA ${process.env.PORT || 3000}`)
-};
+app.listen(process.env.PORT || 3001),
+  () => {
+    logger.infor(`API RODANDO NA PORTA ${process.env.PORT || 3000}`);
+  };
--- backend/src/app/Models/transitionModel.js
+++ backend/src/app/Models/transitionModel.js
@@ -1,33 +1,30 @@
 const mongoose = require('../../config/db'); // Importa o arquivo de configuração do banco de dados
 
-const TransitionSchema = mongoose.Schema(
+const TransactionSchema = mongoose.Schema(
   {
     date: {
+      type: Date,
+      required: true,
+    }, // Recebe a data da transação
+    sellerName: {
       type: String,
       required: true,
-    },
-    sealer: {
-      type: String,
-      required: true,
-    },
+    }, // Recebe o nome do estabelecimento
     value: {
-      type: String,
+      type: Number,
       required: true,
-    },
-    operation:{
-        type: String,
-        required: true,
-    }
+    }, // Sempre receberá um valor positivo, em caso de débito verificar como convertê-lo para negativo
+    operationId: {
+      type: Number,
+      required: true,
+    }, // Recebe 0 para débito e 1 para crédito
   },
   {
     timestamps: true,
   }
 ); // Configuração dos dados que vão para o banco de dados através do Schema da biblioteca mongoose
 
-
-
-const Transition = mongoose.model('Transition', TransitionSchema); // Cria o modelo a partir da configuração
-module.exports = Transition; // Exporta o modelo
-
+const Transaction = mongoose.model('Transaction', TransactionSchema); // Cria o modelo a partir da configuração
+module.exports = Transaction; // Exporta o modelo
 
 // Modelo incial para abstração.
